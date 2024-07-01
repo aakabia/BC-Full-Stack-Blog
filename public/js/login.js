5f54173bcfe331b3c09fbd0538b71b043574d7d5
Aziz Kabia <158849915+aakabia@users.noreply.github.com>
2024-07-01 15:25:57 -0400

    home routes complete

--- public/js/login.js
+++ public/js/login.js
@@ -1,1 +1,68 @@
-console.log("hello");
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+  event.stopPropagation();
+
+  
+
+  const email = document.querySelector("#email").value.trim();
+  const password = document.querySelector("#password").value.trim();
+
+  const alertDiv = document.querySelector("#alert");
+
+  // Above, we grap all our input variables
+
+  if (!email || !password) {
+    const h5EL = document.createElement("h5");
+    h5EL.classList.add("alert");
+    h5EL.textContent = "Please Fill All Entries!";
+
+    alertDiv.append(h5EL);
+
+    setTimeout(() => {
+      h5EL.remove(); // Removes the alert element after 5 seconds.
+    }, 2000);
+    return;
+  }
+
+  // Above, we chack if all our selected elements exist.
+
+ 
+
+  const response = await fetch("/api/users/login", {
+    method: "POST",
+    body: JSON.stringify({ email, password }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace("/");
+  } else {
+    alert("Failed to log in due to incorrect Email or Password!");
+  }
+
+
+  // Above, is to log in for the user.
+};
+
+function showPassword() {
+  const iconSpan = document.querySelector("#iconSpan");
+  const passwordInput = document.querySelector("#password");
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    iconSpan.innerHTML = '<i class="fas fa-eye-slash"></i>'; // Change icon to show crossed eye
+  } else {
+    passwordInput.type = "password";
+    iconSpan.innerHTML = '<i class="fas fa-eye"></i>'; // Change icon to show regular eye
+  }
+}
+// Above, is a function to show the user password.
+
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .querySelector("#logInBtn")
+    .addEventListener("click", loginFormHandler);
+
+  document.querySelector("#iconSpan").addEventListener("click", showPassword);
+});
+
+// Above, we wait till the document is loaded before adding any of our listeners.
--- seeds/user.js
+++ seeds/user.js
@@ -1,55 +1,70 @@
-const { User } = require('../models');
-
+const { User } = require("../models");
+const argon2 = require("argon2");
 // Above we require the user model.
 
-const UserData = [ 
-    {
-        user_name:"aakabia",
-        email:"azizkabia26@yahoo.com",
-        password:"Sunshine95!",
-       
-    },
+const UserData = [
+  {
+    user_name: "aakabia",
+    email: "azizkabia26@yahoo.com",
+    password: "Sunshine95!",
+  },
 
-    {
-        user_name:"Rhique",
-        email:"Enrique732@gmail.com",
-        password:"loveCode%",
-       
-    },
+  {
+    user_name: "Rhique",
+    email: "Enrique732@gmail.com",
+    password: "loveCode%",
+  },
 
+  {
+    user_name: "Sasha338",
+    email: "shedaBOSS@ymail.com",
+    password: "BO$$girl@",
+  },
 
-    {
-        user_name:"Sasha338",
-        email:"shedaBOSS@ymail.com",
-        password:"BO$$girl@",
-       
-    },
+  {
+    user_name: "MiahSays",
+    email: "mariahcultre29@gmail.com",
+    password: "mimi2001",
+  },
 
-    {
-        user_name:"MiahSays",
-        email:"mariahcultre29@gmail.com",
-        password:"mimi2001",
-       
-    },
-
-
-    {
-        user_name:"theCoderKid",
-        email:"codeNode#",
-        password:"Sunshine95!",
-       
-    },
-
-
-]
+  {
+    user_name: "theCoderKid",
+    email: "codeNode#",
+    password: "Sunshine95!",
+  },
+];
 // Above is the data we will use to seed our user table.
 
+const hashUserPasswords = async (UserData) => {
+  try {
+    const hashedUserData = await Promise.all(
+      UserData.map(async (user) => {
+        const hashedPassword = await argon2.hash(user.password);
+        return { ...user, password: hashedPassword };
+      })
+    );
 
+    return hashedUserData; // Optionally return the hashed data if needed
+  } catch (error) {
+    console.error("Error hashing passwords:", error);
+    throw error; // Propagate the error for further handling
+  }
+};
 
+// Above, we hash all the pw before creating new users. Argon2 does not has individually when using bulk create.
+// So we use a function to hash each item password and retunr it back to us using map. 
 
-const seedUserData = () => User.bulkCreate(UserData);
-// Above is our function to bulkcreate our user data.
+const seedUserData = async () => {
+  try {
+    const usersWithHashedPasswords = await hashUserPasswords(UserData);
+    await User.bulkCreate(usersWithHashedPasswords);
+  } catch (error) {
+    console.error("Error seeding users:", error);
+    throw error; // Propagate the error for further handling
+  }
+};
+// Above is our function to bulkcreate our user data with our new hashed pw.
 
 module.exports = seedUserData;
 
-// Above, we export our seedUserData function 
+// Above, we export our seedUserData function
